fb62dd0cfae0d620cc8bf7116ed1da1ed726daaa
fatmakhaledosman <fatma1khaled1@gmail.com>
2024-08-01 11:46:14 +0300

    MySQL Labs

---
+++ lab1 Solution/lab1.sql
@@ -0,0 +1,26 @@
+-- 1.Install MYSQL DBMS.
+
+
+
+-- The scenario is that as an instructor in ITI, you have grade-keeping responsibilities.
+-- You want to convert the grading process from a manual operation using a gradebook to an
+-- electronic representation using database. In this case:
+-- • For each student, you keep track his name, contact info (email, address), 
+--   and multiple phone numbers.
+-- • Each student study different subjects such as (C, CPP, HTML, …), there some students 
+--   study (C,CPP only), and the other students study (HTML,JS only), and so on
+-- • For each subject, you need to define the name and the description and max score.
+-- • The students achieve score in subject by exam result.
+-- • For each exam which taken by student you must store Exam date, score in exam.
+-- • Keep track of subjects which will studied by each student
+-- 2.Design ERD and write down the mapping schema.
+
+
+
+
+-- 3.Create your mapped tables with their columns.
+
+
+
+
+-- 4.Insert your classmates data(5 rows for each table).
---
+++ lab3-2 Solution/lab3-2.sql
@@ -0,0 +1,49 @@
+-- 1. Display the date of exam as the following: day 'month name' year.
+
+
+-- 2. Display the name of students with the year of Birthdate
+
+
+-- 3. Add new exam result, in date column use NOW
+
+
+-- 4. Create Hello world function which take username and return welcome
+--    message to user using his name
+
+
+-- 5. Create multiply function which take two number and return the multiply of them
+
+
+-- 6. Create function which takes number and return if this number is odd or even.
+
+
+-- 7. Create AddNewStudent function which take Student firstName and lastname
+--    and birthdate and TrackName and add this new student info at database
+
+
+-- 8. Create function which takes StudentId and return the string/text that
+--    describe the use info(firstname, last name, TrackName).
+
+
+-- 9. Create function which takes student id and subject id and return score the
+--    student in subject.
+
+
+-- 10.How to Import SQL file into your database
+
+
+-- 11.Create Table called Archive_Students which will hold the deleted students
+--    info(same columns as in student tables)
+
+
+-- 12.Create trigger to save the deleted student from Student table to
+--    Deleted_Students.
+
+
+-- 13.Create trigger to save the newly added students to Student table to
+--    Backup_Students.
+
+
+-- 14.Create trigger to keep track the changes of Tracks table (update rows); it will
+--    logs the time of action and description of action to another Logs table.
+
